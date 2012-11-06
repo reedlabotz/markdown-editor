4478c8259a887f03700a391b4ea2ce106209264b
Reed La Botz <reedlabotz@gmail.com>
2012-11-05 21:38:02 -0600

    Lets play follow the cursor

--- web/markdown.dart
+++ web/markdown.dart
@@ -1,6 +1,8 @@
 import 'dart:html';
 import 'package:dartdoc/markdown.dart' as markdown;
 
+const LETTERS = "abcdefghijklmnopqrstuvwxyz";
+
 TextAreaElement editor;
 DivElement preview;
 
@@ -11,10 +13,34 @@ void main() {
   editor.on.change.add((e) => watchDocument());
   editor.on.input.add((e) => watchDocument());
   editor.on.keyDown.add(keyPressed);
+  
+  editor.focus();
 }
 
 void watchDocument(){
-  preview.innerHTML = markdown.markdownToHtml(editor.value);
+  var value = editor.value;
+  var cursor = editor.selectionStart;
+  
+  var i = cursor;
+  if(i >= value.length){
+    i = value.length -1;
+  }
+  while(i > -1 && !LETTERS.contains(value[i].toLowerCase())){
+    i--;
+  }
+  i++;
+  
+  value = "${value.substring(0,i)}◈${value.substring(i,value.length)}";
+  
+  final document = new markdown.Document();
+  final lines = value.split('\n');
+  final blocks = document.parseLines(lines);
+  var output = markdown.renderToHtml(blocks);
+  output = output.replaceFirst("◈", "<span id='cursor' class='cursor'>|</span>");
+  preview.innerHTML = markdown.markdownToHtml(output);
+  
+  Element cursorDiv = preview.query("#cursor");
+  cursorDiv.scrollIntoView(true);
 }
 
 void keyPressed(KeyboardEvent e){
